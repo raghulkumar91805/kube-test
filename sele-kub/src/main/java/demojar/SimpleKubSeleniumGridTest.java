f399b509ace40fac60e7596ff448e0c3f691a079
raghulkumar91805 <47386593+raghulkumar91805@users.noreply.github.com>
2019-02-13 16:13:36 +0530

    Update SimpleKubSeleniumGridTest.java

--- sele-kub/src/main/java/demojar/SimpleKubSeleniumGridTest.java
+++ sele-kub/src/main/java/demojar/SimpleKubSeleniumGridTest.java
@@ -20,16 +20,12 @@ import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class SimpleKubSeleniumGridTest {
 
 	WebDriver driver;
 	String url="https://www.google.com/";
 	Properties pro;
-	WebDriverWait wait;
-	
 	@Before
 	public void setUp() throws Exception {
 		try{
@@ -42,8 +38,6 @@ public class SimpleKubSeleniumGridTest {
 			System.out.println("Exception is=="+e.getMessage());
 		}
 		DesiredCapabilities dcp = new DesiredCapabilities();
-		//dcp.setCapability(CapabilityType.BROWSER_NAME, BrowserType.CHROME);correct
-		//dcp.setCapability(CapabilityType.PLATFORM, Platform.LINUX);correct
 		dcp.setCapability(CapabilityType.BROWSER_NAME, BrowserType.CHROME);
 		dcp.setCapability(CapabilityType.PLATFORM, Platform.LINUX);
 		dcp.setCapability(CapabilityType.SUPPORTS_APPLICATION_CACHE, true);
@@ -57,7 +51,6 @@ public class SimpleKubSeleniumGridTest {
 		/*System.setProperty("webdriver.chrome.driver",pro.getProperty("chrome.driver").toString());
 		//Open browser instance
 		driver = new ChromeDriver();*/
-		wait = new WebDriverWait(driver,1000);
 		driver.get(url);
 		System.out.println("Opening Google");
 		Thread.sleep(100);
@@ -67,17 +60,15 @@ public class SimpleKubSeleniumGridTest {
 	public void test() throws Exception {
 		System.out.println("Page Title:"+driver.getTitle());
 		//driver.findElement(By.id("lst-ib")).sendKeys("Kubernetes");
-		//driver.findElement(By.id(pro.getProperty("searchbox"))).sendKeys(pro.getProperty("searchword"));//works
-		wait.until(ExpectedConditions.elementToBeClickable(By.id(pro.getProperty("searchbox")))).sendKeys(pro.getProperty("searchword"));
+		driver.findElement(By.id(pro.getProperty("searchbox"))).sendKeys(pro.getProperty("searchword"));
 		System.out.println("Data entered to search");
 		Thread.sleep(100);
 		//driver.findElement(By.name("btnK")).click();
 		//driver.findElement(By.id("lst-b")).sendKeys(Keys.ENTER);
-		//driver.findElement(By.id(pro.getProperty("searchbox"))).sendKeys(Keys.ENTER);//works
-		wait.until(ExpectedConditions.elementToBeClickable(By.id(pro.getProperty("searchbox")))).sendKeys(Keys.ENTER);
+		driver.findElement(By.id(pro.getProperty("searchbox"))).sendKeys(Keys.ENTER);
 		System.out.println("search clicked");
-		Cookie cookie = new Cookie("zaleniumTestPassed", "true");
-	    driver.manage().addCookie(cookie);
+		//Cookie cookie = new Cookie("zaleniumTestPassed", "true");
+	    //driver.manage().addCookie(cookie);
 		Thread.sleep(100);
 	}
 
